929396b418129d75086fab242b4bb979a846ea08
Abián <abian.camejo@gmail.com>
2023-10-09 15:27:36 +0100

    mobile adjustment of info style

--- src/components/IpInfo/IpInfo.jsx
+++ src/components/IpInfo/IpInfo.jsx
@@ -7,27 +7,36 @@ function IpInfo() {
     <>
       <div
         style={{
-          border: "solid",
-          width: "270px",
-          marginLeft: "20%",
-          marginTop: "20%",
-          zIndex: "9999",
-          position: "absolute",
-          textAlign: "center",
-          color: "white",
-          backgroundColor: "white",
-          padding: "10px",
-          borderRadius: "10px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
+
         }}
       >
-        <span className="title">IP ADDRESS</span>
-        <p className="content">192.212.174.101</p>
-        <span className="title">LOCATION</span>
-        <p className="content">Brooklyn, NY 10001</p>
-        <span className="title">TIMEZONE</span>
-        <p className="content">UTC-05:00</p>
-        <span className="title">ISP</span>
-        <p className="content">SpaceX Starlink</p>
+        <div
+          style={{
+            border: "solid",
+            width: "270px",
+            justifyContent: "center",
+            zIndex: "9999",
+            position: "absolute",
+            textAlign: "center",
+            color: "white",
+            backgroundColor: "white",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+        >
+          <span className="title">IP ADDRESS</span>
+          <p className="content">192.212.174.101</p>
+          <span className="title">LOCATION</span>
+          <p className="content">Brooklyn, NY 10001</p>
+          <span className="title">TIMEZONE</span>
+          <p className="content">UTC-05:00</p>
+          <span className="title">ISP</span>
+          <p className="content">SpaceX Starlink</p>
+        </div>
       </div>
     </>
   );
