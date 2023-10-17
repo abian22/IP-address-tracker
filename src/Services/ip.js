80258016de888e23608ade84cf29dbb0c2dc5f11
Abián <70582564+abian22@users.noreply.github.com>
2023-10-17 10:50:28 +0100

    deploy fix2

--- src/Services/ip.js
+++ src/Services/ip.js
@@ -1,6 +1,6 @@
 import api from "./config"
 async function getIp(ip) {
-      const result = await api.get(`/${ip}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query`);
+      const result = await api.get(`/${ip}`);
       console.log("Respuesta de la API:", result);
       return result
 }
